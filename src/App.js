9260939b9f0bcf96c253456332b5f038008c40c1
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-17 13:35:53 -0400

    set up front end

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div></div>;
+  return (
+    <div className="bg-green-50">
+      <h1 className="font-bold text-2xl text-center text-red-900"> Hello </h1>
+    </div>
+  );
 }
 
 export default App;
