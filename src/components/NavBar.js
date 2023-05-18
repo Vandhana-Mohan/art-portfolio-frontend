8911ef14ab9b381c20f40bc318dceb4f1088be6b
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-18 19:45:43 -0400

    navbar css fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,37 +3,43 @@ import Art_Logo from "../assets/Art_Logo.png";
 
 function NavBar() {
   return (
-    <nav className="bg-red-900 flex flex-col sm:flex-row justify-around rounded shadow-red-500/50 shadow-xl">
+    <nav className="sticky top-0 bg-red-900 flex flex-col sm:flex-row items-center justify-between rounded shadow-red-500/50 shadow-xl px-6 py-3">
       <Link to="/">
         <img
           src={Art_Logo}
           alt="Shop_Logo"
-          className="w-32 h-32 shadow-green-500/50 shadow-lg object-contain"
+          className="w-16 h-16 object-cover"
         />
       </Link>
-      <ul className="flex flex-col sm:flex-row mt-6 justify-around">
-        <li className="cursor-pointer hover:text-gray-300 text-xl font-bold mb-6">
-          <Link to="/" className="bg-transparent text-white py-2 px-4 rounded">
+      <ul className="flex flex-col sm:flex-row mt-4 sm:mt-0">
+        <li className="mx-2 sm:mx-4">
+          <Link
+            to="/"
+            className="text-white hover:text-gray-300 text-xl font-bold"
+          >
             Examples
           </Link>
         </li>
-        <li className="cursor-pointer hover:text-gray-300 text-xl font-bold mb-6">
-          <Link to="/" className="bg-transparent text-white py-2 px-4 rounded">
+        <li className="mx-2 sm:mx-4">
+          <Link
+            to="/"
+            className="text-white hover:text-gray-300 text-xl font-bold"
+          >
             My Collection
           </Link>
         </li>
-        <li className="cursor-pointer hover:text-gray-300 mr-4 text-xl font-bold mb-6">
+        <li className="mx-2 sm:mx-4">
           <Link
             to="/groceries/new"
-            className="bg-transparent text-white py-2 px-4 rounded"
+            className="text-white hover:text-gray-300 text-xl font-bold"
           >
             Add New Collection
           </Link>
         </li>
-        <li className="cursor-pointer hover:text-gray-300 mr-4 text-xl font-bold mb-6">
+        <li className="mx-2 sm:mx-4">
           <Link
             to="/groceries/new"
-            className="bg-transparent text-white py-2 px-4 rounded"
+            className="text-white hover:text-gray-300 text-xl font-bold"
           >
             Add Image to Collection
           </Link>
@@ -48,14 +54,14 @@ export default NavBar;
 /* 
 
 <li className="cursor-pointer hover:text-gray-300 mr-4 text-xl font-bold mb-6">
-    <Link
-      to="/categories"
-      className="bg-transparent text-white py-2 px-4 rounded"
-    >
-      Categories
-    </Link>
+  <Link
+    to="/categories"
+    className="bg-transparent text-white py-2 px-4 rounded"
+  >
+    Categories
+  </Link>
 </li>
-      <TotalItems />
-      <Search /> 
+  <TotalItems />
+  <Search /> 
 
 */
