bfeb4c86d748c84d59235e16b4646d6ca1c18c0f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-21 19:02:37 -0400

    updated filter

--- src/Common/FilterButtons.js
+++ src/Common/FilterButtons.js
@@ -42,76 +42,81 @@ function FilterButtons() {
 
   return (
     <div className="flex flex-wrap items-center p-4 m-4">
-      <h2 className="mr-2 text-black text-2xl font-bold py-2 px-4 rounded-lg">
-        Filter:
-      </h2>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-2"
-        onClick={() => setSelectedTheme("All")}
-      >
-        All
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Art")}
-      >
-        Art
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Creative Direction")}
-      >
-        Creative Direction
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Design")}
-      >
-        Design
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Designer")}
-      >
-        Fashion
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Graphic Design")}
-      >
-        Graphic Design
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Illustration")}
-      >
-        Illustration
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Marketing")}
-      >
-        Marketing
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Sports")}
-      >
-        Sports
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Photography")}
-      >
-        Photography
-      </button>
-      <button
-        className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
-        onClick={() => setSelectedTheme("Architecture")}
-      >
-        Architecture
-      </button>
+      {!theme ? (
+        <>
+          <h2 className="mr-2 text-black text-2xl font-bold py-2 px-4 rounded-lg">
+            Filter:
+          </h2>
 
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-2"
+            onClick={() => handleThemeClick("All")}
+          >
+            All
+          </button>
+
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Art")}
+          >
+            Art
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Creative Direction")}
+          >
+            Creative Direction
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Design")}
+          >
+            Design
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Designer")}
+          >
+            Fashion
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Graphic Design")}
+          >
+            Graphic Design
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Illustration")}
+          >
+            Illustration
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Marketing")}
+          >
+            Marketing
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Sports")}
+          >
+            Sports
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Photography")}
+          >
+            Photography
+          </button>
+          <button
+            className="btn bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg m-1"
+            onClick={() => handleThemeClick("Architecture")}
+          >
+            Architecture
+          </button>
+        </>
+      ) : null}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-lg shadow-green-500/50 border shadow-md">
         {theme
           ? filteredData.map((item) => <FilterTheme key={item.id} cat={item} />)
@@ -122,102 +127,3 @@ function FilterButtons() {
 }
 
 export default FilterButtons;
-
-/*
-  return (
-    
-    <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-      {!category ? (
-        <>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md ">
-          <button
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-            onClick={() => handleCategoryClick("Dairy")}
-          >
-            Dairy
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button
-            onClick={() => handleCategoryClick("Vegetables")}
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-          >
-            Vegetables
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button
-            onClick={() => handleCategoryClick("fruits")}
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-          >
-            Fruits
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button
-            onClick={() => handleCategoryClick("Bakery")}
-            className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide"
-          >
-            Bakery
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Canned Goods
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Meat
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Beverages
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide ">
-            Snacks
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col items-center">
-        <div className="bg-green-50 rounded-full w-40 h-40 flex items-center justify-center rounded-lg shadow-green-500/50 shadow-md">
-          <button className="mt-2 font-bold text-black-100 text-center text-3xl tracking-wide">
-            Frozen Foods
-          </button>
-        </div>
-      </div>
-      </>
-      ) : null }
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 rounded-lg shadow-green-500/50 border shadow-md">
-        {category
-          ? filteredProducts.map((item) => (
-              <FilterCategory key={item.id} cat={item} />
-            ))
-          : null}
-      </div>
-    </div>
-  );
-}
-
-
-
-
-*/
--- src/components/ImagesComponents/FilterTheme.js
+++ src/components/ImagesComponents/FilterTheme.js
@@ -2,13 +2,10 @@ import { Link } from "react-router-dom";
 
 function FilterTheme({ cat }) {
   return (
-    <div
-      key={cat.id}
-      className="rounded-lg shadow-green-500/50 border shadow-md overflow-hidden"
-    >
+    <div className="rounded-lg border shadow-md overflow-hidden">
       <Link
         to={`/art/${cat.id}`}
-        className="py-2 px-4 rounded-lg shadow-green-500/50 shadow-md w-max transition-colors duration-300"
+        className="py-2 px-4 rounded-lg bg-green-500 bg-opacity-50 shadow-md w-max transition-colors duration-300"
       >
         {cat.image_url ? (
           <img
@@ -26,33 +23,68 @@ function FilterTheme({ cat }) {
       </Link>
       <div className="p-4 text-center">
         <Link to={`/art/${cat.id}`} className="text-blue-500 hover:underline">
-          <h2 className="text-2xl font-bold mb-2">
+          <h2 className="text-2xl font-bold mb-6">
+            Title:{" "}
             {cat.title
               .split(" ")
-              .map((word) => word.slice(0, 1).toUpperCase() + word.slice(1))
+              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
               .join(" ")}
           </h2>
         </Link>
 
-        <p className="text-lg mb-2">
-          {cat.description
-            ? cat.description.charAt(0).toUpperCase() + cat.description.slice(1)
-            : "Description : Not Available"}
-        </p>
-
-        <div className="flex flex-wrap mb-4 justify-center">
-          <p className="text-gray-600 text-base mr-2">Category:</p>
-          <p className="text-lg">
-            {cat.theme
-              ? cat.theme.charAt(0).toUpperCase() + cat.theme.slice(1)
-              : "Not Available"}
+        <div className="flex items-center mb-8">
+          <strong className="text-lg">Description: </strong>
+          <p className="text-lg ml-2">
+            {cat.description
+              ? cat.description.charAt(0).toUpperCase() +
+                cat.description.slice(1)
+              : "Description: Not Available"}
           </p>
         </div>
+
+        <div className="flex items-center mb-8">
+          <strong className="text-lg">Theme: </strong>
+          <p className="text-lg ml-2">
+            {cat.theme ? cat.theme : "Not Available"}
+          </p>
+        </div>
+
+        <div className="flex items-center mb-8">
+          <strong className="text-lg">Medium / Material: </strong>
+          <p className="text-lg ml-2">
+            {cat.medium ? cat.medium : "Not Available"}
+          </p>
+        </div>
+
+        <div className="flex items-center mb-8">
+          <strong className="text-lg">Image Created On: &nbsp;</strong>
+          <p className="text-lg">
+            {new Date(cat.created_at).toLocaleDateString("en-US", {
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </p>
+        </div>
+
+        {cat.is_for_sale ? (
+          <div className="flex items-center mb-8">
+            <strong className="text-lg">For Sale, &nbsp;</strong>
+            <p className="text-lg">
+              <strong>Price: </strong>
+              {cat.price} USD
+            </p>
+          </div>
+        ) : (
+          <p className="text-lg mb-4">
+            <strong>Not For Sale</strong>
+          </p>
+        )}
 
         <div className="flex flex-wrap mb-4 justify-center">
           <Link
             to={`/art/${cat.id}`}
-            className="py-2 px-4 mt-6 bg-green-300 hover:bg-green-100 text-xl font-bold flex focus:outline-none focus:shadow-outline rounded-lg shadow-green-500/50 shadow-md w-max transition-colors duration-300 text-center"
+            className="py-2 px-4 mt-6 bg-green-300 hover:bg-green-100 text-xl font-bold flex focus:outline-none focus:shadow-outline rounded-lg shadow-md w-max transition-colors duration-300 text-center"
           >
             View Details
           </Link>
@@ -63,22 +95,3 @@ function FilterTheme({ cat }) {
 }
 
 export default FilterTheme;
-
-/*
-    <div className="flex flex-wrap mb-4 justify-center">
-          <p className="text-gray-600 text-base mr-2">Price:</p>
-          <p className="text-lg">{cat.price} USD</p>
-        </div> 
-        <div className="flex flex-wrap mb-4 justify-center">
-          <p className="text-gray-600 text-base mr-2">Quantity:</p>
-          <p className="text-lg">
-            {cat.quantity} {cat.unit}
-          </p>
-        </div> 
-
-       <div className="flex flex-wrap mb-4 justify-center">
-          <p className="text-gray-600 text-base mr-2">Organic:</p>
-          <p className="text-lg">{cat.is_organic ? "Yes" : "No"}</p>
-        </div>
-
-        */
