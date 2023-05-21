a1a0f8fe2e14961c2d6f406b6bc15200f3dc6cdd
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-20 21:51:03 -0400

    sign in

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-
 function SignIn() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -7,11 +6,17 @@ function SignIn() {
     <div>
       <form>
         <h1>Log In</h1>
-        <input type="email" placeholder="Enter your Email" value={email} />
+        <input
+          type="email"
+          placeholder="Enter your Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
         <input
           type="password"
           placeholder="Enter your Password"
           value={password}
+          onChange={(event) => setPassword(event.target.value)}
         />
       </form>
     </div>
